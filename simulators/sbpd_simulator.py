c8b1c2e652313b9b1efcce18b3d0a68f0d5deab2
sdeglurkar <sampada_deglurkar@berkeley.edu>
2022-10-30 17:47:37 -0700

    Cost is of trajectory and not point

--- simulators/sbpd_simulator.py
+++ simulators/sbpd_simulator.py
@@ -1,7 +1,8 @@
+from multiprocessing.dummy import current_process
 import numpy as np
 from obstacles.sbpd_map import SBPDMap
 from simulators.simulator import Simulator
-from trajectory.trajectory import Trajectory
+from trajectory.trajectory import Trajectory, SystemConfig
 
 
 class SBPDSimulator(Simulator):
@@ -39,24 +40,31 @@ class SBPDSimulator(Simulator):
         the MPC problem.
         '''
         costmap = []  # Just scalars
-        # The map origin of the FMM map is [0, 0] and the optimal_waypoint_ego_n3 is in relative
-        # coordinates with the ego position
-        waypoints = data_dict['optimal_waypoint_n3']  # TODO (sdeglurkar): Should it really be ego n3 
+        current_states = data_dict['vehicle_state_nk3'][:, 0]  # First state in 30-step episode
+        waypoints = data_dict['optimal_waypoint_n3']   
         goal_positions = data_dict['goal_position_n2']
         for i in range(len(goal_positions)):
+            current_pos = current_states[i, :2]
+            current_heading = current_states[i, 2]
             goal_pos = goal_positions[i].reshape(1, 2)
             waypt_pos = waypoints[i, :2]
             waypt_heading = waypoints[i, 2]
             # Get the FMM map with this goal position
             fmm_map = self._init_fmm_map(goal_pos)
             self._update_obj_fn(fmm_map)
+
             n = 1  # Batch size
             k = 1  # 1-step "trajectories" -- just waypoints
-            one_step_trajectories = Trajectory(dt=0, n=n, k=k, 
-                                                position_nk2=waypt_pos.reshape((n, k, 2)), 
-                                                heading_nk1=waypt_heading.reshape((n, k, 1)))
-            one_step_trajectories.update_valid_mask_nk()  # Needed for taking a valid mean of objective values
-            cost = self.obj_fn.evaluate_function(one_step_trajectories)  # Tensor
+            start_config = SystemConfig(dt=0, n=n, k=k, 
+                                        position_nk2=current_pos.reshape((n, k, 2)), 
+                                        heading_nk1=current_heading.reshape((n, k, 1)))
+            goal_config = SystemConfig(dt=0, n=n, k=k, 
+                                        position_nk2=waypt_pos.reshape((n, k, 2)),
+                                        heading_nk1=waypt_heading.reshape((n, k, 1)))
+            # Take spline trajectory from current robot state to waypoint
+            # and take the cost of that
+            cost, _ = self.planner.eval_objective(start_config, goal_config)  # Tensor
+            print("COST", cost)
             cost = cost[0].numpy()
             costmap.append(cost)  
         
