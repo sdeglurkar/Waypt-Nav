2cc3e0b1485a55bc3e38aa79afd9c0e55c915267
sdeglurkar <sampada_deglurkar@berkeley.edu>
2022-11-10 13:27:59 -0800

    Fixing some bugs in testing

--- planners/nn_waypoint_cost_planner.py
+++ planners/nn_waypoint_cost_planner.py
@@ -7,11 +7,92 @@ from trajectory.trajectory import Trajectory, SystemConfig
 class NNWaypointCostPlanner(NNPlanner):
     """ A planner which selects an optimal waypoint using
     a trained neural network. Is basically a wrapper for 
-    NNWaypointPlanner. """
+    NNWaypointPlanner to account for the cost field in the
+    data dictionary. """
 
     def __init__(self, simulator, params):
         super(NNWaypointCostPlanner, self).__init__(simulator, params)
         self.waypoint_world_config = SystemConfig(dt=self.params.dt, n=1, k=1)
+
+    @staticmethod
+    def empty_data_dict():
+        """Returns a dictionary with keys mapping to empty lists
+        for each datum computed by a planner."""
+        data = {'system_config': [],
+                'waypoint_config': [],
+                'waypoint_cost': [],
+                'trajectory': [],
+                'spline_trajectory': [],
+                'planning_horizon': [],
+                'K_nkfd': [],
+                'k_nkf1': [],
+                'img_nmkd': []}
+        return data
+    
+    @staticmethod
+    def mask_and_concat_data_along_batch_dim(data, k):
+        """Keeps the elements in data which were produced
+        before time index k. Concatenates each list in data
+        along the batch dim after masking. Also returns data
+        from the first segment not in the valid mask."""
+
+        # Extract the Index of the Last Data Segment
+        data_times = np.cumsum([traj.k for traj in data['trajectory']])
+        valid_mask = (data_times <= k)
+        data_last = {}
+        last_data_idxs = np.where(np.logical_not(valid_mask))[0]
+
+        # Take the first last_data_idx
+        if len(last_data_idxs) > 0:
+            last_data_idx = last_data_idxs[0]
+            last_data_valid = True
+        else:
+            # Take the last element as it is not valid anyway
+            last_data_idx = len(valid_mask) - 1
+            last_data_valid = False
+
+        # Get the last segment data
+        data_last['system_config'] = data['system_config'][last_data_idx]
+        data_last['waypoint_config'] = data['waypoint_config'][last_data_idx]
+        data_last['waypoint_cost'] = data['waypoint_cost'][last_data_idx]
+        data_last['trajectory'] = data['trajectory'][last_data_idx]
+        data_last['spline_trajectory'] = data['spline_trajectory'][last_data_idx]
+        data_last['planning_horizon_n1'] = [data['planning_horizon'][last_data_idx]] 
+        data_last['K_nkfd'] = data['K_nkfd'][last_data_idx]
+        data_last['k_nkf1'] = data['k_nkf1'][last_data_idx]
+        data_last['img_nmkd'] = data['img_nmkd'][last_data_idx]
+
+        # Get the main planner data
+        data['system_config'] = SystemConfig.concat_across_batch_dim(np.array(data['system_config'])[valid_mask])
+        data['waypoint_config'] = SystemConfig.concat_across_batch_dim(np.array(data['waypoint_config'])[valid_mask])
+        data['waypoint_cost'] = np.array(data['waypoint_cost'])[valid_mask]
+        data['trajectory'] = Trajectory.concat_across_batch_dim(np.array(data['trajectory'])[valid_mask])
+        data['spline_trajectory'] = Trajectory.concat_across_batch_dim(np.array(data['spline_trajectory'])[valid_mask])
+        data['planning_horizon_n1'] = np.array(data['planning_horizon'])[valid_mask][:, None]
+        data['K_nkfd'] = tf.boolean_mask(tf.concat(data['K_nkfd'], axis=0), valid_mask)
+        data['k_nkf1'] = tf.boolean_mask(tf.concat(data['k_nkf1'], axis=0), valid_mask)
+        data['img_nmkd'] = np.array(np.concatenate(data['img_nmkd'], axis=0))[valid_mask]
+        return data, data_last, last_data_valid
+    
+    @staticmethod
+    def convert_planner_data_to_numpy_repr(data):
+        """
+        Convert any tensors into numpy arrays in a
+        planner data dictionary.
+        """
+        if len(data.keys()) == 0:
+            return data
+        data_numpy = {}
+        data_numpy['system_config'] = data['system_config'].to_numpy_repr()
+        data_numpy['waypoint_config'] = data['waypoint_config'].to_numpy_repr()
+        data_numpy['waypoint_cost'] = data['waypoint_cost']
+        data_numpy['trajectory'] = data['trajectory'].to_numpy_repr()
+        data_numpy['spline_trajectory'] = data['spline_trajectory'].to_numpy_repr()
+        data_numpy['planning_horizon_n1'] = data['planning_horizon_n1']
+        data_numpy['K_nkfd'] = data['K_nkfd'].numpy()
+        data_numpy['k_nkf1'] = data['k_nkf1'].numpy()
+        data_numpy['img_nmkd'] = data['img_nmkd']
+        return data_numpy
 
     def _raw_data(self, start_config):
         """
@@ -89,7 +170,7 @@ class NNWaypointCostPlanner(NNPlanner):
 
         data = {'system_config': SystemConfig.copy(start_config),
                 'waypoint_config': SystemConfig.copy(self.opt_waypt),
-                'waypoint_cost': min_cost,
+                'waypoint_cost': min_cost.numpy(),
                 'trajectory': Trajectory.copy(self.opt_traj),
                 'spline_trajectory': Trajectory.copy(trajectories_spline),
                 'planning_horizon': min_horizon,
